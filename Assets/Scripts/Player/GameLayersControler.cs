cefd9f3b55bb2d085d4f8291c1908af7499ed30b
Lorsenok <121055045+Lorsenok@users.noreply.github.com>
2025-07-09 22:07:01 +0300

    game layer system

---
+++ Assets/Scripts/Player/GameLayersControler.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Zenject;
+
+public class GameLayersControler : MonoBehaviour
+{
+    public static int CurrentLayer { get; set; } = 0;
+
+    [SerializeField] private int setLayerOnStart = 0;
+    [SerializeField] private GameLayer[] layers;
+
+    [SerializeField] private float speed;
+
+    [SerializeField] private float zdistance;
+    [SerializeField] private float ydistance;
+
+    private List<Collider2D[]> colliders = new List<Collider2D[]>();
+
+    private InputSystem inputSystem;
+    [Inject] private void Init(IInputControler inputControler)
+    {
+        inputSystem = inputControler.GetInputSystem();
+    }
+
+    private void Start()
+    {
+        CurrentLayer = setLayerOnStart;
+
+        foreach (GameLayer layer in layers)
+        {
+            colliders.Add(layer.GetComponentsInChildren<Collider2D>());
+        }
+    }
+
+    private void OnSwitchUp(InputAction.CallbackContext context)
+    {
+        if (CurrentLayer < layers.Length - 1) CurrentLayer++;
+    }
+
+    private void OnSwitchDown(InputAction.CallbackContext context)
+    {
+        if (CurrentLayer > 0) CurrentLayer--;
+    }
+
+    private void OnEnable()
+    {
+        inputSystem.Player.SwitchLayerUp.performed += OnSwitchUp;
+        inputSystem.Player.SwitchLayerDown.performed += OnSwitchDown;
+    }
+    private void OnDisable()
+    {
+        inputSystem.Player.SwitchLayerUp.performed -= OnSwitchUp;
+        inputSystem.Player.SwitchLayerDown.performed -= OnSwitchDown;
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < layers.Length; i++)
+        {
+            layers[i].transform.position = Vector3.Lerp(layers[i].transform.position, 
+                transform.position + new Vector3(0, ydistance * (i - CurrentLayer), zdistance * (i - CurrentLayer)), 
+                speed * Time.deltaTime);
+
+            for (int j = 0; j < colliders[i].Length; j++)
+            {
+                colliders[i][j].enabled = i == CurrentLayer;
+            }
+        }
+    }
+}
