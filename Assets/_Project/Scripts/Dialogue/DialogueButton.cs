3d0eefe5ff7ecb5eea16b015e6bd43344e8ea24e
Lorsenok <121055045+Lorsenok@users.noreply.github.com>
2025-08-19 18:26:11 +0300

    some fixes
    some ui fixes
    ducks speed debuff
    default material fixes
    made choice button better

--- Assets/_Project/Scripts/Dialogue/DialogueButton.cs
+++ Assets/_Project/Scripts/Dialogue/DialogueButton.cs
@@ -1,5 +1,7 @@
 using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueButton : GameButton
 {
@@ -7,7 +9,26 @@ public class DialogueButton : GameButton
     public static Action<int> OnButtonPressed { get; set; }
 
     [SerializeField] private Timer blockTimer;
+
+    [Header("Question Mark")]
+    [SerializeField] private float questionMarkSpeed = 5f;
+
+    [SerializeField] private RectTransform questionMark;
+    [SerializeField] private Vector3 questionMarkScale = Vector3.one;
+
+    [SerializeField] private Image questionMarkImage;
+    [SerializeField] private Color questionMarkDefaultColor;
+    [SerializeField] private Color questionMarkSelectedColor;
+
+    [Header("Text")]
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private float textColorChangeSpeed;
+    [SerializeField] private Color defaultTextColor = Color.white;
+    [SerializeField] private Color selectedTextColor;
+
     private bool block = true;
+
+    private Vector3 startScale;
 
     public override void OnEnable()
     {
@@ -24,6 +45,30 @@ public class DialogueButton : GameButton
     private void OnBlockEnd()
     {
         block = false;
+    }
+
+    public override void Start()
+    {
+        base.Start();
+        startScale = questionMark.localScale;
+    }
+
+    public void Update()
+    {
+        questionMark.localScale = 
+            Vector3.Lerp(questionMark.localScale, 
+            isMouseOn ? questionMarkScale : startScale, 
+            Time.deltaTime * questionMarkSpeed);
+
+        questionMarkImage.color =
+            Color.Lerp(questionMarkImage.color,
+            isMouseOn ? questionMarkSelectedColor : questionMarkDefaultColor,
+            Time.deltaTime * questionMarkSpeed);
+
+        text.color =
+            Color.Lerp(text.color,
+            isMouseOn ? selectedTextColor : defaultTextColor,
+            Time.deltaTime * textColorChangeSpeed);
     }
 
     public override void Action()
