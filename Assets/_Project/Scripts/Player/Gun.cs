90b1c24bc3ded0dd847c571e31d033281c506e46
Lorsenok <121055045+Lorsenok@users.noreply.github.com>
2025-10-10 20:11:11 +0300

    gun fix

--- Assets/_Project/Scripts/Player/Gun.cs
+++ Assets/_Project/Scripts/Player/Gun.cs
@@ -8,10 +8,31 @@ public class Gun : MonoBehaviour
     [SerializeField] private bool pinToMouse = true;
     
     [SerializeField] private float rotationOffset;
+    [SerializeField] private float[] rotationLocks;
     
     [SerializeField] private Bullet bulletPrefab;
     [SerializeField] private Transform bulletSpawnPoint;
 
+    private float FindClosestRotation(float rotation) //by chatgpt
+    {
+        float closest = rotationLocks[0];
+        float minDiff = 360f;
+
+        foreach (float lockAngle in rotationLocks)
+        {
+            float diff = Mathf.Abs(Mathf.DeltaAngle(rotation, lockAngle));
+
+            if (diff < minDiff)
+            {
+                minDiff = diff;
+                closest = lockAngle;
+            }
+        }
+
+        return closest;
+    }
+
+    
     public void Shoot(float power)
     {
         Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.transform.rotation).Power = power;
@@ -19,6 +40,9 @@ public class Gun : MonoBehaviour
     private void Update()
     {
         Vector2 dir = new Vector2(transform.position.x, transform.position.y) - (pinToMouse ? ProjMath.MousePosition() : Target.position);
-        transform.eulerAngles = new Vector3(0f, 0f, ProjMath.RotateTowardsPosition(dir.normalized) + rotationOffset);
+        float rotation = ProjMath.RotateTowardsPosition(dir.normalized);
+        if (rotationLocks.Length > 0)
+            rotation = FindClosestRotation(rotation);
+        transform.eulerAngles = new Vector3(0f, 0f, rotation + rotationOffset);
     }
 }
