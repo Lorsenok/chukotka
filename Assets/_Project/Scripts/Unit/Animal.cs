a96a9791aafc6ef32e1e0c397361e3c3fc01b262
Napoleonsha <amnoli3847@gmail.com>
2025-10-27 18:13:41 +0200

    Build 2.2

--- Assets/_Project/Scripts/Unit/Animal.cs
+++ Assets/_Project/Scripts/Unit/Animal.cs
@@ -1,80 +1,111 @@
+using System;
 using UnityEngine;
+using Random = System.Random;
 
 public class Animal : TargetFollower
 {
-    [Header("Animal Settings")]
+    [Header("Animal")]
     [SerializeField] private DestroyableObject destroyableObject;
     [SerializeField] private GameObject[] dropPrefab;
 
-    [Header("Escape")]
-    [SerializeField] private float escapeSpeedMultiplier = 1f;
+    [Header("Escaping")][SerializeField] private float escapeSpeedMultiplier = 1f;
     [SerializeField] private int hpWhenEscaping;
     [SerializeField] private Timer timeToEscape;
     [SerializeField] private GameObject[] spawnAfterEscapePrefabs;
 
-    private bool isEscaping = false;
-    private bool aggro = false;
+    [Header("Custom Animator")]
+    [SerializeField] private bool flip = false;
+    [SerializeField] private float minSpeedX = 0.25f;
+    [SerializeField] private CustomAnimatorController animController;
+    [SerializeField] private string idleAnim;
+    [SerializeField] private float idleAnimTime;
+    [SerializeField] private string walkAnim;
+    [SerializeField] private float walkAnimTime;
+    [SerializeField] private string jumpAnim;
+    [SerializeField] private float jumpAnimTime;
 
+    private bool isEscaping = false;
+
+    private bool agr = false;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.GetComponent<Controller>())
-            aggro = true;
+        if (other.gameObject.GetComponent<Controller>())
+        {
+            agr = true;
+        }
     }
 
-    protected override void OnEnable()
+    public override void OnEnable()
     {
         base.OnEnable();
-        if (destroyableObject != null) destroyableObject.OnDie += Drop;
-        if (timeToEscape != null) timeToEscape.OnTimerEnd += OnEscape;
+        destroyableObject.OnDie += Drop;
+        timeToEscape.OnTimerEnd += OnEscape;
     }
 
-    protected override void OnDisable()
+    public override void OnDisable()
     {
         base.OnDisable();
-        if (destroyableObject != null) destroyableObject.OnDie -= Drop;
-        if (timeToEscape != null) timeToEscape.OnTimerEnd -= OnEscape;
+        destroyableObject.OnDie -= Drop;
+        timeToEscape.OnTimerEnd -= OnEscape;
     }
 
     private void Start()
     {
-        if (target == null)
-            target = FindFirstObjectByType<Controller>().transform;
+        target = FindObjectsByType<Controller>(FindObjectsSortMode.None)[0].transform;
     }
 
-    protected override void Update()
+    public override void Update()
     {
-        if (destroyableObject != null && destroyableObject.HP <= 0) return;
-        if (!aggro) return;
-
+        if (destroyableObject.HP <= 0) return;
+        bool isMoving = rg.linearVelocityX > minVelocityToMove || rg.linearVelocityX < -minVelocityToMove;
+        if (!agr)
+        {
+            return;
+        }
         if (destroyableObject.HP <= hpWhenEscaping && !isEscaping)
         {
             isEscaping = true;
-            if (timeToEscape != null) timeToEscape.StartTimer();
+            timeToEscape.StartTimer();
         }
-
         if (isEscaping)
-            rg.linearVelocity = new Vector2(-escapeSpeedMultiplier * speed, rg.linearVelocity.y);
-        else
-            base.Update();
+        {
+            Move(-escapeSpeedMultiplier);
+            if (groundChecker.IsTouchingGround && canJump) Jump();
+        }
+        else base.Update();
+
+        spr.flipX = target.position.x < transform.position.x ? flip : !flip;
+
+        if (isMoving && animController != null && Mathf.Abs(rg.linearVelocityX) > minSpeedX && Vector2.Distance(transform.position, target.position) > minDistance)
+        {
+            animController.PullAnimation(walkAnim, walkAnimTime);
+        }
+        else if (animController != null)
+        {
+            animController.PullAnimation(idleAnim, idleAnimTime);
+        }
     }
 
-    protected override void Jump(float jumpHeight)
+    public override void Jump()
     {
-        base.Jump(jumpHeight);
+        base.Jump();
+        if (animController != null) animController.PullAnimation(jumpAnim, jumpAnimTime);
     }
 
     private void OnEscape()
     {
         if (!isEscaping) return;
-        foreach (var obj in spawnAfterEscapePrefabs)
-            Instantiate(obj, transform.position, transform.rotation);
+        foreach (GameObject obj in spawnAfterEscapePrefabs)
+            Instantiate(obj, transform.position, obj.transform.rotation);
         Destroy(gameObject);
     }
 
     private void Drop()
     {
-        if (dropPrefab.Length == 0) return;
-        int rand = Random.Range(0, dropPrefab.Length);
-        Instantiate(dropPrefab[rand], transform.position, dropPrefab[rand].transform.rotation);
+        if (dropPrefab.Length != 0)
+        {
+            int rand = UnityEngine.Random.Range(0, dropPrefab.Length);
+            Instantiate(dropPrefab[rand], transform.position, dropPrefab[rand].transform.rotation);
+        }
     }
 }
--- Assets/_Project/Scripts/Unit/TargetFollower.cs
+++ Assets/_Project/Scripts/Unit/TargetFollower.cs
@@ -1,117 +1,102 @@
+using System;
 using UnityEngine;
 
 public class TargetFollower : MonoBehaviour
 {
-    [Header("Target & Rigidbody")]
+    public float SpeedMultiplier { get; set; } = 1f;
+
     [SerializeField] protected Transform target;
     [SerializeField] protected Rigidbody2D rg;
     [SerializeField] protected GroundChecker groundChecker;
 
-    [Header("Movement Settings")]
-    [SerializeField] protected float speed = 5f;
-    [SerializeField] protected float acceleration = 20f;
-    [SerializeField] protected float deceleration = 15f;
-    [SerializeField] protected float minDistance = 0.1f;
+    [Header("Speed")]
+    [SerializeField] protected float minDistance;
+    [SerializeField] protected float deceleration;
+    [SerializeField] protected float speed;
+    [SerializeField] protected float acceleration;
 
-    [Header("Jump Settings")]
-    [SerializeField] protected float minDistanceToJump = 1f;
-    [SerializeField] protected float jumpForce = 10f;
+    [Header("Jump")]
+    [SerializeField] protected float jumpForce;
     [SerializeField] protected Timer jumpDelayTimer;
+    [SerializeField] protected float minDistanceToJump;
 
     [Header("Animation")]
     [SerializeField] protected CustomAnimator idle;
     [SerializeField] protected CustomAnimator move;
     [SerializeField] protected SpriteRenderer spr;
-    [SerializeField] protected float minVelocityToMove = 0.1f;
-
-    public float SpeedMultiplier { get; set; } = 1f;
-    protected bool canJump = true;
-
-    public void Stop()
-    {
-        if (rg != null)
-            rg.linearVelocity = new Vector2(0f, rg.linearVelocity.y); // останавливаем только горизонтальное движение
-    }
-
-    protected virtual void OnEnable()
-    {
-        if (jumpDelayTimer != null)
-            jumpDelayTimer.OnTimerEnd += OnJumpDelayEnd;
-    }
-
-    protected virtual void OnDisable()
-    {
-        if (jumpDelayTimer != null)
-            jumpDelayTimer.OnTimerEnd -= OnJumpDelayEnd;
-    }
-
-    private void OnJumpDelayEnd()
-    {
-        canJump = true;
-    }
+    [SerializeField] protected float minVelocityToMove;
 
     public void SetTarget(Transform newTarget)
     {
         target = newTarget;
     }
 
-    protected virtual void Update()
+    public virtual void OnEnable()
     {
-        if (target == null) return;
-
-        HandleMovement();
-        HandleJump();
-        HandleAnimation();
+        jumpDelayTimer.OnTimerEnd += OnJumpDelayEnd;
     }
 
-    private void HandleMovement()
+    public virtual void OnDisable()
     {
-        float directionX = target.position.x - transform.position.x;
-
-        float desiredVelocityX = 0f;
-        if (Mathf.Abs(directionX) > minDistance)
-            desiredVelocityX = Mathf.Sign(directionX) * speed * SpeedMultiplier;
-
-        rg.linearVelocity = new Vector2(
-            Mathf.MoveTowards(rg.linearVelocity.x, desiredVelocityX, acceleration * Time.deltaTime),
-            rg.linearVelocity.y
-        );
+        jumpDelayTimer.OnTimerEnd -= OnJumpDelayEnd;
     }
 
-    private void HandleJump()
+    protected bool canJump = true;
+    private void OnJumpDelayEnd()
     {
-        if (!canJump || target == null) return;
-
-        float distanceY = target.position.y - transform.position.y;
-
-        if (distanceY > minDistanceToJump && groundChecker.IsTouchingGround)
-        {
-            Jump(distanceY);
-        }
+        canJump = true;
     }
 
-    protected virtual void Jump(float jumpHeight)
+    public void Stop()
     {
-        // Сброс вертикальной скорости перед прыжком
-        rg.linearVelocity = new Vector2(rg.linearVelocity.x, 0f);
+        rg.linearVelocityX = 0f;
+    }
 
-        // Расчет силы прыжка для достижения цели
-        float jumpVelocity = Mathf.Sqrt(2f * Mathf.Abs(Physics2D.gravity.y) * jumpHeight);
-        rg.AddForce(Vector2.up * jumpVelocity, ForceMode2D.Impulse);
+    public void Move(float multiplier)
+    {
+        rg.linearVelocityX += (target.position - transform.position).normalized.x * acceleration * Time.deltaTime * multiplier * SpeedMultiplier;
+        rg.linearVelocityX = Mathf.Clamp(rg.linearVelocityX, -speed, speed);
+    }
 
+    public virtual void Jump()
+    {
+        rg.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         canJump = false;
-        if (jumpDelayTimer != null)
-            jumpDelayTimer.StartTimer();
+        jumpDelayTimer.StartTimer();
     }
 
-    private void HandleAnimation()
+    private float ignoreAnimationsTime = 0f;
+    private CustomAnimator lastAnimator;
+    public void PullOtherAnimation(CustomAnimator animator, float time)
     {
-        bool isMoving = Mathf.Abs(rg.linearVelocity.x) > minVelocityToMove;
+        lastAnimator = animator;
+        animator.enabled = true;
+        ignoreAnimationsTime = time;
+    }
 
+    public virtual void Update()
+    {
+        rg.linearVelocityX = Mathf.Lerp(rg.linearVelocityX, 0f, deceleration * Time.deltaTime);
+        if (Vector2.Distance(transform.position, target.position) > minDistance)
+        {
+            Move(1f);
+        }
+
+        if (canJump && groundChecker.IsTouchingGround && target.position.y - transform.position.y >= minDistanceToJump)
+        {
+            Jump();
+        }
+
+        if (ignoreAnimationsTime > 0f)
+        {
+            ignoreAnimationsTime -= Time.deltaTime;
+            return;
+        }
+
+        if (lastAnimator != null) lastAnimator.enabled = false;
+        bool isMoving = rg.linearVelocityX > minVelocityToMove || rg.linearVelocityX < -minVelocityToMove;
         if (idle != null) idle.enabled = !isMoving;
         if (move != null) move.enabled = isMoving;
-
-        if (spr != null && isMoving)
-            spr.flipX = rg.linearVelocity.x < 0f;
+        if (spr != null && isMoving) spr.flipX = rg.linearVelocityX < 0f;
     }
 }
