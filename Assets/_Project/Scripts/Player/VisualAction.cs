a2d9e236ca4453357cdc99838f7a1e8542d7e4ac
Lorsenok <121055045+Lorsenok@users.noreply.github.com>
2025-11-18 21:34:08 +0300

    visual actions implementation

--- Assets/_Project/Scripts/Player/VisualAction.cs
+++ Assets/_Project/Scripts/Player/VisualAction.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Item", menuName = "Scriptable Objects/Item")]
+[CreateAssetMenu(fileName = "Item", menuName = "Scriptable Objects/Visual Action")]
 public class VisualAction : ScriptableObject
 {
     public float shakePower;
