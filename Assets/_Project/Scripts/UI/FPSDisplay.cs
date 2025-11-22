dbd7ed21d84d8e42160ea9d3367bd7a003e99811
Zhekach <62952369+Zhekach@users.noreply.github.com>
2025-11-22 21:42:07 +0300

    add fps script

---
+++ Assets/_Project/Scripts/UI/FPSDisplay.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSDisplay : MonoBehaviour
+{
+    private const int NormalFrameRate = 50;
+    private const string FPSPrefix = "FPS: ";
+    
+    [SerializeField] private Text fpsText;
+    [SerializeField] private float updateInterval = 0.2f;
+    [SerializeField] private Color normalColor = Color.green;
+    [SerializeField] private Color lowFpsColor = Color.red;
+
+    private float _accumulatedTime;
+    private int _frames;
+    private float _timer;
+
+    // Здесь мы не рассчитываем FPS каждый кадр — это шумно.
+    // Мы собираем данные и обновляем вывод раз в updateInterval.
+    private void Update()
+    {
+        _accumulatedTime += Time.unscaledDeltaTime;
+        _frames++;
+        _timer += Time.unscaledDeltaTime;
+
+        if (_timer >= updateInterval)
+        {
+            int fps = Mathf.RoundToInt(_frames / _accumulatedTime);
+            
+            fpsText.text = FPSPrefix + fps;
+            fpsText.color = CalculateColor(fps);
+
+            _timer = 0f;
+            _frames = 0;
+            _accumulatedTime = 0f;
+        }
+    }
+    
+    private Color CalculateColor(float fps)
+    {
+        return fps < NormalFrameRate ? lowFpsColor : normalColor;
+    }
+}
