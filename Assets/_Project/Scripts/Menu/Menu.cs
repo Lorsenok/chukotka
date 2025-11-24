1aea8ab62de87e47ac53f5c874829401e8ba1a83
Lorsenok <121055045+Lorsenok@users.noreply.github.com>
2025-11-24 19:08:15 +0300

    Revert "стоп игры на паузе"
    This reverts commit 9c80c721aa9c5afc4247f7c658e125eda1a1ece6.

--- Assets/_Project/Scripts/Menu/Menu.cs
+++ Assets/_Project/Scripts/Menu/Menu.cs
@@ -1,29 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class Menu : MonoBehaviour 
-{ 
-    public string MenuName; 
-    public bool Open; 
-    [SerializeField] private float speed = 5f; 
-    [SerializeField] private Vector3 openPosition; 
-    [SerializeField] private Vector3 closePosition; 
-     private Coroutine moveCoroutine; 
-    public void ChangeState(bool state) 
-    { 
-        Open = state;  
-        if (Open) Time.timeScale = 0f; 
-        else Time.timeScale = 1f;  
-        if (moveCoroutine != null) StopCoroutine(moveCoroutine); 
-        moveCoroutine = StartCoroutine(MoveMenu(Open ? openPosition : closePosition)); 
-    } 
-    private IEnumerator MoveMenu(Vector3 targetPosition) 
-    { 
-        while (Vector3.Distance(transform.localPosition, targetPosition) > 0.01f) 
-        { 
-            transform.localPosition = Vector3.Lerp( transform.localPosition, targetPosition, Time.unscaledDeltaTime * speed); 
-            yield return null; 
-        } 
-        transform.localPosition = targetPosition; 
-    } 
+
+public class Menu : MonoBehaviour
+{
+    public string MenuName;
+    public bool Open;
+
+    [SerializeField] private float speed;
+    [SerializeField] private Vector3 openPosition;
+    [SerializeField] private Vector3 closePosition;
+
+    public void ChangeState(bool state)
+    {
+        Open = state;
+    }
+
+    private void Update()
+    {
+        transform.localPosition = Vector3.Lerp(transform.localPosition, Open ? openPosition : closePosition, Time.deltaTime * speed);
+    }
 }
