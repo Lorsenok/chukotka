6d60e363a6c2b3057292bec4de6ca73dc80efc0d
Napoleonsha <amnoli3847@gmail.com>
2025-11-25 04:28:33 +0200

    спавнер животных
    нормально работает спавн животных

--- Assets/_Project/Scripts/Unit/Spawners/AnimalSpawner.cs
+++ Assets/_Project/Scripts/Unit/Spawners/AnimalSpawner.cs
@@ -1,60 +1,142 @@
-using System;
+using System.Collections;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class AnimalSpawner : Spawner
+public class AnimalSpawner : MonoBehaviour
 {
-    private Vector3 startPosition;
-    [Header("Animal Spawner")]
-    
-    [SerializeField] private bool lockToTarget;
+    [Header("Prefabs & Tags")]
+    [SerializeField] private GameObject birdPrefab;
+    [SerializeField] private string birdTag = "Bird"; 
+    [SerializeField] private GameObject wolfPrefab;
+    [SerializeField] private string wolfTag = "Wolf";
+
+    [Header("Spawn area")]
+    [SerializeField] private bool lockToTarget = false;
     [SerializeField] private Transform target;
-    
-    [SerializeField] private float maxRange;
-    [SerializeField] private float minRange;
-    [SerializeField] private Timer changePositionTimer;
+    [SerializeField] private float maxRange = 20f;
+    [SerializeField] private float minRange = 2f;
+    [SerializeField] private float startCheckDelay = 1f;
 
-    public override void OnEnable()
-    {
-        base.OnEnable();
-        changePositionTimer.OnTimerEnd += OnPositionChange;
-    }
+    [Header("Timers")]
+    [SerializeField] private float birdCheckInterval = 30f;  
+    [SerializeField] private int birdSpawn = 6;          
+    [SerializeField] private int birdSpawnMin = 1;           
+    [SerializeField] private int birdSpawnMax = 3;
 
-    public override void OnDisable()
-    {
-        base.OnDisable();
-        changePositionTimer.OnTimerEnd -= OnPositionChange;
-    }
+    [SerializeField] private float wolfCheckInterval = 30f;  
+    [SerializeField] private int wolfSpawn = 2;          
 
-    private void OnPositionChange()
-    {
-        int maxIterations = 100;
-        float curRange = 0f;
-        while (curRange < minRange & curRange > -minRange ||
-               startPosition.x + curRange > BorderController.MaxDistance |
-               startPosition.x + curRange < BorderController.MinDistance)
-        {
-            curRange = Random.Range(-maxRange, maxRange);
-            maxIterations--;
-            if (maxIterations <= 0)
-            {
-                Debug.LogError("Too many tries to set position in range!");
-                return;
-            }
-        }
-        transform.position =
-            new Vector3(startPosition.x + curRange, startPosition.y, startPosition.z);
-    }
+    private Vector3 startPosition;
+    private Coroutine birdRoutine;
+    private Coroutine wolfRoutine;
 
-    public void Start()
+    private void Start()
     {
         startPosition = transform.position;
+        StartCoroutine(StartRoutinesDelayed(startCheckDelay));
     }
 
-    public void Update()
+    private IEnumerator StartRoutinesDelayed(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        birdRoutine = StartCoroutine(BirdChecker());
+        wolfRoutine = StartCoroutine(WolfChecker());
+    }
+
+    private IEnumerator BirdChecker()
+    {
+        while (true)
+        {
+            int current = CountByTag(birdTag);
+            if (current < birdSpawn)
+            {
+                int toSpawn = Random.Range(birdSpawnMin, birdSpawnMax + 1);
+                for (int i = 0; i < toSpawn; i++)
+                {
+                    Vector3 pos = GetValidSpawnPosition();
+                    SpawnPrefab(birdPrefab, pos);
+                }
+            }
+            yield return new WaitForSeconds(birdCheckInterval);
+        }
+    }
+
+    private IEnumerator WolfChecker()
+    {
+        while (true)
+        {
+            int current = CountByTag(wolfTag);
+            if (current < wolfSpawn)
+            {
+                SpawnPrefab(wolfPrefab, GetValidSpawnPosition());
+            }
+            yield return new WaitForSeconds(wolfCheckInterval);
+        }
+    }
+
+    private int CountByTag(string tag)
+    {
+        try
+        {
+            var objs = GameObject.FindGameObjectsWithTag(tag);
+            return objs.Length;
+        }
+        catch
+        {
+            return 0;
+        }
+    }
+
+    private Vector3 GetValidSpawnPosition()
+    {
+        float curRange = 0f;
+        int maxIter = 100;
+        Vector3 candidate = startPosition;
+
+        while (maxIter-- > 0)
+        {
+            curRange = Random.Range(-maxRange, maxRange);
+
+            if (Mathf.Abs(curRange) < minRange) continue;
+
+            float x = startPosition.x + curRange;
+            candidate = new Vector3(x, startPosition.y, startPosition.z);
+            return candidate;
+        }
+
+        return startPosition;
+    }
+
+    private void SpawnPrefab(GameObject prefab, Vector3 position)
+    {
+        Instantiate(prefab, position, Quaternion.identity);
+    }
+
+    private void Update()
     {
         if (!lockToTarget) return;
-        if (!target) target = FindObjectsByType<Controller>(FindObjectsSortMode.None)[0].transform;
+
+        if (target == null)
+        {
+            var controllers = FindObjectsByType<Controller>(FindObjectsSortMode.None);
+            if (controllers.Length > 0)
+                target = controllers[0].transform;
+            else
+                return;
+        }
+
         startPosition = target.position;
+    }
+
+    private void OnDisable()
+    {
+        if (birdRoutine != null) StopCoroutine(birdRoutine);
+        if (wolfRoutine != null) StopCoroutine(wolfRoutine);
+    }
+
+    public void OnPositionChange()
+    {
+        startPosition = GetValidSpawnPosition();
+        transform.position = startPosition;
     }
 }
